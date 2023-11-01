8f5bd328e4cef2502fb24837f10faf519dcf5ba2
jacketdembys <utdyrh@mail.missouri.edu>
2023-11-01 23:28:07 +0000

    running experiments

--- ea-based-nn-ik-solver/create_experiments.sh
+++ ea-based-nn-ik-solver/create_experiments.sh
@@ -2,14 +2,15 @@
 # This script creates a config YAML file
 # List all the config files
 layers=1
-neurons=10
-scripts=$(ls config_layers_"$layers"_neurons_"$neurons"/*.yaml)
+neurons=1000
+robot_choice="7DoF-7R-Panda"
+scripts=$(ls configs/"$robot_choice"/config_layers_"$layers"_neurons_"$neurons"/*.yaml)
 
 # Create the config file
 echo "$scripts";
 
 for script in $scripts; do
-    python ea-nn-ik-solver-pytorch-6DoF.py --config-path "$script" &
+    python ik-solver.py --config-path "$script" &
 done
 
 
